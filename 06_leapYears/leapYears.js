820892f9757c5013cf0092cbd87e5d976b6e4f67
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-22 13:07:28 -0600

    06 and 07 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const divBy = (a,n) => a%n===0;
 
+const leapYears = function(year) {
+    return (
+        divBy(year,4) && (!divBy(year,100) || (divBy(year,100) && divBy(year,400)))
+    );
 };
 
 // Do not edit below this line
