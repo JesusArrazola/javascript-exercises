02dffc5315254f0b67c8bb36371add3cc554c444
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-22 11:40:09 -0600

    03, 04 and 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    if( (a<0||b<0)|| (typeof a !=='number' || typeof b !== 'number')){
+        return "ERROR";
+    }
 
+    if(b<a){
+        let aux = a;
+        a = b;
+        b = aux;
+    }
+
+    for(let i=a;i<=b;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
