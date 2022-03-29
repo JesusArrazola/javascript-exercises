520faa50cd54420f0a378075a2476ac8dca2cffb
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-29 10:49:21 -0600

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    const regExp = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    const spaceRE = /\s+/g;
+    pal = pal.replace(regExp,'').replace(spaceRE,"").toLowerCase();
+    let j = pal.length - 1;
+    for(let i = 0;i<pal.length;i++){
+        if(pal.charAt(i) === pal.charAt(j)){
+            j--;
+            continue;
+        }else{
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
