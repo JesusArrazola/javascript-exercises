1d27d2af346ab65c985e96eeadf49dde878cc719
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-30 11:29:06 -0600

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+/**
+ * 
+ * @param {Array} people 
+ * @returns Object
+ */
+const findTheOldest = function(people) {
+    return people.reduce((acc,curr)=>{
+        //console.log(acc,curr);
+        if(!acc) return curr;
+        if(!("yearOfDeath" in curr)){
+            curr.yearOfDeath = new Date().getFullYear();
+        }
+        if(!("yearOfDeath" in acc)){
+            acc.yearOfDeath = new Date().getFullYear();
+        }
+        return (curr.yearOfDeath-curr.yearOfBirth)>=(acc.yearOfDeath-acc.yearOfBirth)?curr:acc;
+    },null);
 };
 
 // Do not edit below this line
