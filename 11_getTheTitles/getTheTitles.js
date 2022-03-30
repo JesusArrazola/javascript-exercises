081c5719bf498692c8ff78e75c720bd8e3b68e2b
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-30 07:37:24 -0600

    Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+/**
+ * Returns an array containing only the titles of the books
+ * @param {Array} books The Array of books
+ * @returns Array
+ */
+const getTheTitles = function(books) {
+    return books.map(book=>book.title);
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
